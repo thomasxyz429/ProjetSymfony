7efca05b94e1f7a7d803413bebf98bb0a212851b
toto <tgcyberdragon@gmail.com>
2021-01-10 11:04:00 +0100

    Mise en place des pages statiques

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -17,4 +17,24 @@ class ProStageController extends AbstractController
             'controller_name' => 'ProStageController',
         ]);
     }
+
+    /**
+     * @Route("/entreprises", name="pro_stage_entreprises")
+     */
+    public function affichageEntreprises(): Response
+    {
+        return $this->render('pro_stage/affichageEntreprises.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+
+    /**
+     * @Route("/formations", name="pro_stage_formations")
+     */
+    public function affichageformations(): Response
+    {
+        return $this->render('pro_stage/affichageformations.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
 }
