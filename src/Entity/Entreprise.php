238d22f1c943bdb72de43c004dfe8d2418fdd61c
toto <tgcyberdragon@gmail.com>
2021-01-11 19:55:51 +0100

    initialisation

---
+++ src/Entity/Entreprise.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EntrepriseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EntrepriseRepository::class)
+ */
+class Entreprise
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $idEntreprise;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     */
+    private $milieu;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdEntreprise(): ?int
+    {
+        return $this->idEntreprise;
+    }
+
+    public function setIdEntreprise(int $idEntreprise): self
+    {
+        $this->idEntreprise = $idEntreprise;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getMilieu(): ?string
+    {
+        return $this->milieu;
+    }
+
+    public function setMilieu(?string $milieu): self
+    {
+        $this->milieu = $milieu;
+
+        return $this;
+    }
+}
