cb5e2706243b78a59cf0faa7ec23bb4082ab2d4c
toto <tgcyberdragon@gmail.com>
2021-01-12 15:38:55 +0100

    Installation Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,17 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
+use  App\Entity\Entreprise;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $IBM = new Entreprise();
+        $IBM->setIdEntreprise (IBM0);
+        $IBM->setNom ("IBM");
+        $IBM->setMilieu('Programmation et Industrie Fine');
+        $IBM->setAdresse('IBM@contactStage.fr');
+        $manager->persist($IBM);
 
         $manager->flush();
     }
